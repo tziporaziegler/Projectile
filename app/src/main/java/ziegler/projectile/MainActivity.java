005a9a8360190e2e5e0e1c6fc6d2e2602d3031c6
tziporaziegler <tziporaziegler@gmail.com>
2015-09-20 17:07:32 -0400

    Initial Commit

---
+++ app/src/main/java/ziegler/projectile/MainActivity.java
@@ -0,0 +1,90 @@
+package ziegler.projectile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.Formatter;
+
+public class MainActivity extends AppCompatActivity {
+    EditText angleText;
+    EditText velocityText;
+    EditText timeText;
+    Button calculateButton;
+    TextView answerView;
+
+    private double angle;
+    private double velocity;
+    private double time;
+    private double radians;
+
+    private DecimalFormat formatter = new DecimalFormat("#,##0.##");
+
+    public String getX() {
+        return formatter.format(Math.sin(radians) * velocity * time);
+    }
+
+    public String getY() {
+        return formatter.format(Math.cos(radians) * velocity * time - (.5 * 9.8 * time * time));
+    }
+
+    public double getTime(){
+        return time;
+    }
+
+    public void setTime() {
+        time += 0.1;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        angleText = (EditText) findViewById(R.id.angle);
+        velocityText = (EditText) findViewById(R.id.velocity);
+        timeText = (EditText) findViewById(R.id.time);
+        answerView = (TextView) findViewById(R.id.answer);
+
+        calculateButton = (Button) findViewById(R.id.calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                angle = Double.valueOf(angleText.getText().toString());
+                velocity = Double.valueOf(velocityText.getText().toString());
+                time = Double.valueOf(timeText.getText().toString());
+                radians = Math.toRadians(angle);
+                String answer = "(" + getX() + ", " + getY() + ")";
+                answerView.setText(answer);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
